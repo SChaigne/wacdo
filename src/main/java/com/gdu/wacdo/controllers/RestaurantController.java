4fd6532785dd0a9e49ad37edd35b2ccb27ceb920
SCH <djpix44@gmail.com>
2025-11-03 11:34:15 +0100

    restaurant - add rsearch with param

--- src/main/java/com/gdu/wacdo/controllers/RestaurantController.java
+++ src/main/java/com/gdu/wacdo/controllers/RestaurantController.java
@@ -1,8 +1,10 @@
 package com.gdu.wacdo.controllers;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,9 +13,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.gdu.wacdo.models.Affectation;
 import com.gdu.wacdo.models.Restaurant;
 import com.gdu.wacdo.models.RestaurantDto;
+import com.gdu.wacdo.services.AffectationRepository;
+import com.gdu.wacdo.services.JobRepository;
 import com.gdu.wacdo.services.RestaurantsRepository;
 
 import jakarta.validation.Valid;
@@ -24,28 +30,44 @@ public class RestaurantController {
 
 	@Autowired
 	private RestaurantsRepository restaurantsRepository;
+	@Autowired
+	private JobRepository jobRepository;
+	@Autowired
+	private AffectationRepository affectationsRepository;
 
 	@GetMapping({ "", "/" })
-	public String restaurants(Model model) {
-		List<Restaurant> listOfRestaurants = restaurantsRepository.findAll();
+	public String restaurants(@RequestParam(required = false) String name,
+			@RequestParam(required = false) String zipCode, @RequestParam(required = false) String city, Model model) {
 
-		model.addAttribute("restaurants", listOfRestaurants); // restaurants , ici on associe a la variable dans mon											// template
+		List<Restaurant> restaurants = restaurantsRepository.searchRestaurants(name, zipCode, city);
 
-		return "restaurants/index"; // template thymeleaf
+		model.addAttribute("restaurants", restaurants);
+		model.addAttribute("name", name);
+		model.addAttribute("zipCode", zipCode);
+		model.addAttribute("city", city);
+
+		return "restaurants/index";
 	}
 
 	@GetMapping("/{id}")
-	public String restaurantDetail(@PathVariable Long id, Model model) {
-		try {
-			Restaurant restaurant = restaurantsRepository.findById(id)
-					.orElseThrow(() -> new RuntimeException("Restaurant non trouvé avec id: " + id));
+	public String restaurantDetail(@PathVariable Long id, @RequestParam(required = false) Long jobId,
+			@RequestParam(required = false) String name,
+			@RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date startDate, Model model) {
 
-			model.addAttribute("restaurant", restaurant);
-			return "restaurants/detail";
-		} catch (Exception ex) {
-			System.out.println("Exception: " + ex.getMessage());
-			return "redirect:/restaurants";
-		}
+		Restaurant restaurant = restaurantsRepository.findById(id)
+				.orElseThrow(() -> new RuntimeException("Restaurant not found"));
+
+		List<Affectation> affectations = affectationsRepository.findCurrentAffectationsByRestaurant(id, jobId, name,
+				startDate);
+
+		model.addAttribute("restaurant", restaurant);
+		model.addAttribute("affectations", affectations);
+		model.addAttribute("jobs", jobRepository.findAll());
+		model.addAttribute("jobId", jobId);
+		model.addAttribute("name", name);
+		model.addAttribute("startDate", startDate);
+
+		return "restaurants/detail";
 	}
 
 	@GetMapping("/create")
