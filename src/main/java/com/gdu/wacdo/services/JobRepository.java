a8886ea5dc318a87da87bc3034c9a1e8af157e36
SCH <djpix44@gmail.com>
2025-11-07 18:39:51 +0100

    Rename package

--- src/main/java/com/gdu/wacdo/services/JobRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gdu.wacdo.services;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gdu.wacdo.models.Job;
-
-public interface JobRepository extends JpaRepository<Job, Long>{
-
-}
